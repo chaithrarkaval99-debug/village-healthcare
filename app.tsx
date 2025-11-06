451a6c96ab58b0b92b8a8075a82060234c1dd946
chaithrarkaval99-debug <chaithrarkaval99@gmail.com>
2025-11-06 14:55:18 +0530

    Create app.tsx

---
+++ app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatInterface from './chatinterface';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <ChatInterface />
+    </div>
+  );
+}
+
+export default App;
